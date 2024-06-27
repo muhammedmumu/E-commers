78928ac413601e3949300130f2a5c1f161227559
Muhammed Mukrim.M <mugammedmukrim786@gmail.com>
2024-06-27 23:52:17 +0530

    Add files via upload

---
+++ Front-end/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import  './DescriptionBox.css';
+
+const DescriptionBox = () => {
+return (
+
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+        </div>
+                <div className="descriptionbox-description">
+                    <p>
+                    Do you need a beautiful and unique dress for your next special occasion? Look no further than this chiffon bridesmaid dress! This gorgeous dress features a scoop neckline, lace chiffon fabric, and a hi-lo hemline that is sure to turn heads. The 94% nylon and 6% spandex fabric are comfortable and figure-flattering, and the hand wash-only care instructions make it easy to keep this dress looking its best. The zipper closure type ensures a perfect fit, and the imported origin means you can be confident in the quality of this dress. Whether you’re attending a wedding, a party, or a formal event, this floral lace chiffon dress is a perfect choice.
+                    </p>
+                    <p>
+                    This elegant three-piece suit is excellent for any formal occasion. Do you want to feel like George Clooney on the red carpet? Our slim fit and single-breasted blazer will give you a sharp and polished look while the pants and vest complete the ensemble. The 80% polyester and 20% viscose fabric is comfortable and easy to care for, and we recommend dry cleaning for the best results.
+                    </p>
+                </div>
+            </div>
+)
+}
+export default DescriptionBox;
